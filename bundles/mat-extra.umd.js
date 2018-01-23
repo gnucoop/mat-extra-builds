0df0b174f19b934a3653bb27ba167b52e2b8b787
trik <marco.marche@gnucoop.com>
2018-01-23 16:34:59 +0100

    36db3eb first commit

---
+++ bundles/mat-extra.umd.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2018 Gnucoop soc. coop.
+ * 
+ * This file is part of ng-extra.
+ * 
+ * ng-extra is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ng-extra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ng-extra.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core')) :
+	typeof define === 'function' && define.amd ? define(['exports', '@angular/core'], factory) :
+	(factory((global.ng = global.ng || {}, global.ng['mat-extra'] = global.ng['mat-extra'] || {}),global.ng.core));
+}(this, (function (exports,_angular_core) { 'use strict';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * Copyright (C) 2018 Gnucoop soc. coop.
+ *
+ * This file is part of ng-extra.
+ *
+ * ng-extra is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ng-extra is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ng-extra.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/**
+ * Current version of Ng-Extra.
+ */
+var VERSION = new _angular_core.Version('0.1.0-36db3eb');
+
+exports.VERSION = VERSION;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=mat-extra.umd.js.map
