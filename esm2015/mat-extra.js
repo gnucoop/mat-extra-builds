b09a0c63d3230bca7eb977276014546f1808f19d
trik <marco.marche@gnucoop.com>
2018-01-24 14:35:10 +0000

    0966875 fix(e2e): take screenshot in dummy test

--- esm2015/mat-extra.js
+++ esm2015/mat-extra.js
@@ -45,7 +45,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of Ng-Extra.
  */
-const VERSION = new Version('0.1.0-629781c');
+const VERSION = new Version('0.1.0-0966875');
 
 /**
  * @fileoverview added by tsickle
