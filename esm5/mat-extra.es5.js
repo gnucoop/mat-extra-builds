adb27245e85cca31a89530a3e1aa78df4d562b5a
trik <marco.marche@gnucoop.com>
2018-01-24 10:37:15 +0000

    0b58b8a fix(chore): wrong browserstack config

--- esm5/mat-extra.es5.js
+++ esm5/mat-extra.es5.js
@@ -45,7 +45,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of Ng-Extra.
  */
-var VERSION = new Version('0.1.0-9575053');
+var VERSION = new Version('0.1.0-0b58b8a');
 
 /**
  * @fileoverview added by tsickle
